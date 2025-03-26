a733ccbc36a777e6f786ef53df15e948aa3d999b
Arshpreet0991 <arsh.singh0991@gmail.com>
2025-03-26 13:47:07 -0400

    Database connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import { DB_NAME } from "../constants.js"; // import db name from constant which will form the part of the url
 
+// async function because db can take time to connect
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
+      // get mongodb_uri from .env file and db_name from constant and form a complete url of the database and pass it inside mongoose.connect
     );
     console.log(
       `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host} `
@@ -15,4 +17,4 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
+export default connectDB; // export connectDB so that the index.js from the root folder can access it
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-//require("dotenv").config({ path: "./env" });
-import { app } from "./app.js";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import connectDB from "./db/index.js";
+import dotenv from "dotenv"; // import the dotenv package to get access to .env file. This should be at the top, because index file is the first file to execute and by doing this all the files inside the project will have access to .env variables from the start.
+import connectDB from "./db/index.js"; // import connectDB form DB index file
 
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./env" }); // define the path of the .env
 
 connectDB();
