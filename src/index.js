3b0a9f2c9c68df0efd1e0bf498b9a7da73b26829
Arshpreet0991 <arsh.singh0991@gmail.com>
2025-03-26 13:27:45 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //require("dotenv").config({ path: "./env" });
+import { app } from "./app.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
